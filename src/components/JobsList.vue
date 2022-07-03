f411f3924798311b59b05da9d7a4055c703ecd09
Karan Odedra <karanodedra81@gmail.com>
2022-07-03 13:32:27 +0530

    add transition to the list

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="job-list">
-    <p>Ordered by {{ order }}</p>
-    <ul>
+    <p>
+      Ordered by: <b>{{ order }}</b>
+    </p>
+    <transition-group name="list" tag="ul">
       <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
@@ -16,7 +18,7 @@
           </p>
         </div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -78,5 +80,8 @@ export default defineComponent({
   color: #17bf66;
   font-weight: bold;
   margin: 10px 4px;
+}
+.list-move {
+  transition: all 1s;
 }
 </style>
