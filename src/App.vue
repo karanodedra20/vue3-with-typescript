e45e6ca98faa794c401123a60c4444c7de1d563e
Karan Odedra <karanodedra81@gmail.com>
2022-07-03 12:47:10 +0530

    add Job Interface

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="app">Hello, hyrule</div>
+  <div class="app">
+    <p>{{ jobs[0].location }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
   components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+    return { jobs };
+  },
 });
 </script>
 
